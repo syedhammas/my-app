a70dbb29aee84e07b05b5b1cdf3aec8ecded4015
Syed Hammas <syedhammas786@gmail.com>
2024-12-20 20:01:58 +0500

    Created a React js App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './My components/Header';
+import {Todos} from './My components/Todos';
+import {Footer} from './My components/Footer';
+
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Todos/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/My components/Todoitem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Todoitem = () => {
+  return (
+    <div>
+      Todo works!!
+    </div>
+  )
+}
+
+export default Todo
